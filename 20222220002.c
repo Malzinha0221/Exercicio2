4e3d53f6c1556a0fafc738d2638e733f663d8a71
Daniel Nazario <115184735+Malzinha0221@users.noreply.github.com>
2022-12-21 06:02:27 -0400

    Create 20222220002.c

---
+++ 20222220002.c
@@ -0,0 +1,22 @@
+# include  < stdio.h >
+int  principal (){
+
+int numero,soma;
+
+for ( int i = 1 ; i <= 49 ; i++){
+
+    printf ( " Digite um numero par ou impar: " );
+    scanf ( " %d " ,&numero);
+
+    if (número % 2 != 0 ){
+
+        soma += numero;
+
+    }
+
+}
+
+printf ( " O resultado da soma dos numeros impares e: %d " ,soma);
+
+retorna  0 ;
+}
